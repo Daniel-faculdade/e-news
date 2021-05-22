bc260cd909d875d4bfd6c0e74abb06598003714e
Daniel-faculdade <71900772@aluno.faculdadecotemig.br>
2021-05-22 09:54:04 -0300

    First commit - Ajustes iniciais

--- app/src/main/java/br/com/stockstalk/enews/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/stockstalk/enews/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.stockstalk.enews
+package br.com.stockstalk.enews.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.stockstalk.enews.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
