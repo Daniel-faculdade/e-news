f7c16dc6e7ad926f06fab46a5d40be037711ef4e
JeyDev10 <jvalveslol@gmail.com>
2021-05-27 23:12:33 -0300

    criação home e service

---
+++ app/src/main/java/br/com/stockstalk/enews/models/Account.kt
@@ -0,0 +1,7 @@
+package br.com.stockstalk.enews.models
+
+class Account {
+    var name: String? = ""
+    var email: String? = ""
+    var password: String? = ""
+}
---
+++ app/src/main/java/br/com/stockstalk/enews/ui/activities/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package br.com.stockstalk.enews.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.stockstalk.enews.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+
+}
